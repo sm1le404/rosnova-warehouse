b9c6d6e02091099b41c605f78b41e018a3017ec9
artursirbiladze <xjesterx404@gmail.com>
2023-06-20 15:28:33 +0300

    common service done

--- src/common/services/common.service.ts
+++ src/common/services/common.service.ts
@@ -1,67 +1,50 @@
-// import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import {
-//   DeepPartial,
-//   FindOptionsRelations,
-//   FindOptionsWhere,
-//   Repository,
-// } from 'typeorm';
-// import { CommonEntity } from '../entities/common.entity';
-//
-// @Injectable()
-// export class CommonService<T extends CommonEntity> {
-//   constructor(
-//     @InjectRepository(T)
-//     private commonRepository: Repository<T>,
-//   ) {}
-//
-//   async find(
-//     filter: FindOptionsWhere<T> | FindOptionsWhere<T>[],
-//     relations: FindOptionsRelations<T> | FindOptionsWhere<T> = [],
-//   ): Promise<T[]> {
-//     return this.commonRepository.find({
-//       where: filter,
-//       relations,
-//     });
-//   }
-//
-//   async findOne(
-//     filter: FindOptionsWhere<T>,
-//     relations: FindOptionsRelations<T> | FindOptionsWhere<T> = [],
-//   ): Promise<T> {
-//     return this.commonRepository.findOneOrFail({
-//       where: filter,
-//       relations,
-//     });
-//   }
-//
-//   async create(createCommonEntity: DeepPartial<T>): Promise<T> {
-//     const common = this.commonRepository.create(createCommonEntity);
-//     await this.commonRepository.save(common);
-//     return this.findOne({ id: common.id });
-//   }
-//
-//   async update(
-//     filter: FindOptionsWhere<T>,
-//     updateCommon: DeepPartial<T>,
-//   ): Promise<T> {
-//     const common = await this.findOne(filter);
-//     Object.assign(common, updateCommon);
-//     await this.commonRepository.save(common);
-//     return this.findOne(filter);
-//   }
-//
-//   async delete(filter: FindOptionsWhere<T>): Promise<T> {
-//     const common = await this.findOne(filter);
-//     return this.commonRepository.softRemove(common);
-//   }
-//
-//   async deleteMany(filter: FindOptionsWhere<T>): Promise<T[]> {
-//     const commons = await this.commonRepository.find({ where: filter });
-//     return this.commonRepository.softRemove(commons);
-//   }
-//
-//   async restore(filter: FindOptionsWhere<T>): Promise<void> {
-//     await this.commonRepository.restore(filter);
-//   }
-// }
+import {
+  DeepPartial,
+  FindManyOptions,
+  FindOneOptions,
+  Repository,
+} from 'typeorm';
+import { CommonEntity } from '../entities/common.entity';
+
+export abstract class CommonService<T extends CommonEntity> {
+  abstract getRepository(): Repository<T>;
+
+  async find(payload: FindManyOptions<T>): Promise<T[]> {
+    return this.getRepository().find({
+      ...payload,
+    });
+  }
+
+  async findOne(payload: FindOneOptions<T>): Promise<T> {
+    return this.getRepository().findOneOrFail(payload);
+  }
+
+  async create(createCommonEntity: DeepPartial<T>): Promise<T> {
+    const common = this.getRepository().create(createCommonEntity);
+    return this.getRepository().save(common);
+  }
+
+  async update(
+    filter: FindOneOptions<T>,
+    updateCommon: DeepPartial<T>,
+  ): Promise<T> {
+    const common = await this.findOne(filter);
+    Object.assign(common, updateCommon);
+    return this.getRepository().save(common);
+  }
+
+  async delete(filter: FindOneOptions<T>): Promise<T> {
+    const common = await this.findOne(filter);
+    return this.getRepository().softRemove(common);
+  }
+
+  async deleteMany(payload: FindManyOptions<T>): Promise<T[]> {
+    const commons = await this.find(payload);
+    return this.getRepository().softRemove(commons);
+  }
+
+  async restore(filter: FindOneOptions<T>): Promise<void> {
+    const common = await this.findOne({ ...filter, withDeleted: true });
+    await this.getRepository().recover(common);
+  }
+}
