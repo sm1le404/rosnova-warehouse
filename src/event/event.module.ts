bea47869b90af08fccb18f33660df4c45ea70ae0
Denis <denelvis@yandex.ru>
2023-07-12 12:32:16 +0300

    fix event

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventService } from './services/event.service';
 import { EventController } from './controllers/event.controller';
+import { Event } from './entities/event.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Event])],
