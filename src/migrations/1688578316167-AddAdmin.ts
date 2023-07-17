a31514ee252e61f6481a261bba2b109da32c860e
Denis <denelvis@yandex.ru>
2023-07-17 15:04:22 +0300

    fix: change migrations, rename admin to Администратор

--- src/migrations/1688578316167-AddAdmin.ts
+++ src/migrations/1688578316167-AddAdmin.ts
@@ -5,7 +5,7 @@ export class AddAdmin1688578316167 implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `INSERT INTO "user" ("role", "login", "password")
-          VALUES ('ADMIN', 'admin', '$2b$05$ZId.arIHR5B65t5E4AqhgOHMt4WwK5JaTvasiVs.HREcpp2wGohRy');`,
+          VALUES ('ADMIN', 'Администратор', '$2b$05$ZId.arIHR5B65t5E4AqhgOHMt4WwK5JaTvasiVs.HREcpp2wGohRy');`,
     );
   }
 
