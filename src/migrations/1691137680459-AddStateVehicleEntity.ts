a1dc8157db4a9c3159eb28ea89fa8a28c7c99343
Denis <denelvis@yandex.ru>
2023-08-04 11:29:54 +0300

    feat; add vehicle state

---
+++ src/migrations/1691137680459-AddStateVehicleEntity.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+/*eslint-disable*/
+export class AddStateVehicleEntity1691137680459 implements MigrationInterface {
+  name = 'AddStateVehicleEntity1691137680459';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "temporary_vehicle" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "createdAt" integer NOT NULL DEFAULT (strftime('%s', 'now')), "updatedAt" integer NOT NULL DEFAULT (strftime('%s', 'now')), "deletedAt" datetime, "type" varchar CHECK( "type" IN ('TRUCK','LOADER') ) NOT NULL, "carModel" text NOT NULL, "regNumber" varchar NOT NULL, "isEnabled" boolean NOT NULL DEFAULT (1), "driverId" integer, "trailerId" integer, CONSTRAINT "UQ_211dafa64c1f70a0ef4cc6b77d1" UNIQUE ("trailerId"), CONSTRAINT "UQ_d05630c6efdefc8b7c670c344ac" UNIQUE ("driverId"), CONSTRAINT "FK_822a3a73059d1892341f5c9f8af" FOREIGN KEY ("trailerId") REFERENCES "trailer" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT "FK_cbb46518af7f7bf832253c62e08" FOREIGN KEY ("driverId") REFERENCES "driver" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "temporary_vehicle"("id", "createdAt", "updatedAt", "deletedAt", "type", "carModel", "regNumber", "isEnabled", "driverId", "trailerId") SELECT "id", "createdAt", "updatedAt", "deletedAt", "type", "carModel", "regNumber", "isEnabled", "driverId", "trailerId" FROM "vehicle"`,
+    );
+    await queryRunner.query(`DROP TABLE "vehicle"`);
+    await queryRunner.query(
+      `ALTER TABLE "temporary_vehicle" RENAME TO "vehicle"`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "temporary_vehicle" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "createdAt" integer NOT NULL DEFAULT (strftime('%s', 'now')), "updatedAt" integer NOT NULL DEFAULT (strftime('%s', 'now')), "deletedAt" datetime, "type" varchar CHECK( "type" IN ('TRUCK','LOADER') ) NOT NULL, "carModel" text NOT NULL, "regNumber" varchar NOT NULL, "isEnabled" boolean NOT NULL DEFAULT (1), "driverId" integer, "trailerId" integer, CONSTRAINT "UQ_211dafa64c1f70a0ef4cc6b77d1" UNIQUE ("trailerId"), CONSTRAINT "UQ_d05630c6efdefc8b7c670c344ac" UNIQUE ("driverId"), CONSTRAINT "FK_822a3a73059d1892341f5c9f8af" FOREIGN KEY ("trailerId") REFERENCES "trailer" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT "FK_cbb46518af7f7bf832253c62e08" FOREIGN KEY ("driverId") REFERENCES "driver" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "temporary_vehicle"("id", "createdAt", "updatedAt", "deletedAt", "type", "carModel", "regNumber", "isEnabled", "driverId", "trailerId") SELECT "id", "createdAt", "updatedAt", "deletedAt", "type", "carModel", "regNumber", "isEnabled", "driverId", "trailerId" FROM "vehicle"`,
+    );
+    await queryRunner.query(`DROP TABLE "vehicle"`);
+    await queryRunner.query(
+      `ALTER TABLE "temporary_vehicle" RENAME TO "vehicle"`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "vehicle" RENAME TO "temporary_vehicle"`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "vehicle" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "createdAt" integer NOT NULL DEFAULT (strftime('%s', 'now')), "updatedAt" integer NOT NULL DEFAULT (strftime('%s', 'now')), "deletedAt" datetime, "type" varchar CHECK( "type" IN ('TRUCK','LOADER') ) NOT NULL, "carModel" text NOT NULL, "regNumber" varchar NOT NULL, "isEnabled" boolean NOT NULL DEFAULT (1), "driverId" integer, "trailerId" integer, CONSTRAINT "UQ_211dafa64c1f70a0ef4cc6b77d1" UNIQUE ("trailerId"), CONSTRAINT "UQ_d05630c6efdefc8b7c670c344ac" UNIQUE ("driverId"), CONSTRAINT "FK_822a3a73059d1892341f5c9f8af" FOREIGN KEY ("trailerId") REFERENCES "trailer" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT "FK_cbb46518af7f7bf832253c62e08" FOREIGN KEY ("driverId") REFERENCES "driver" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "vehicle"("id", "createdAt", "updatedAt", "deletedAt", "type", "carModel", "regNumber", "isEnabled", "driverId", "trailerId") SELECT "id", "createdAt", "updatedAt", "deletedAt", "type", "carModel", "regNumber", "isEnabled", "driverId", "trailerId" FROM "temporary_vehicle"`,
+    );
+    await queryRunner.query(`DROP TABLE "temporary_vehicle"`);
+    await queryRunner.query(
+      `ALTER TABLE "vehicle" RENAME TO "temporary_vehicle"`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "vehicle" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "createdAt" integer NOT NULL DEFAULT (strftime('%s', 'now')), "updatedAt" integer NOT NULL DEFAULT (strftime('%s', 'now')), "deletedAt" datetime, "type" varchar CHECK( "type" IN ('TRUCK','LOADER') ) NOT NULL, "carModel" text NOT NULL, "regNumber" varchar NOT NULL, "isEnabled" boolean NOT NULL DEFAULT (1), "driverId" integer, "trailerId" integer, CONSTRAINT "UQ_211dafa64c1f70a0ef4cc6b77d1" UNIQUE ("trailerId"), CONSTRAINT "UQ_d05630c6efdefc8b7c670c344ac" UNIQUE ("driverId"), CONSTRAINT "FK_822a3a73059d1892341f5c9f8af" FOREIGN KEY ("trailerId") REFERENCES "trailer" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT "FK_cbb46518af7f7bf832253c62e08" FOREIGN KEY ("driverId") REFERENCES "driver" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "vehicle"("id", "createdAt", "updatedAt", "deletedAt", "type", "carModel", "regNumber", "isEnabled", "driverId", "trailerId") SELECT "id", "createdAt", "updatedAt", "deletedAt", "type", "carModel", "regNumber", "isEnabled", "driverId", "trailerId" FROM "temporary_vehicle"`,
+    );
+    await queryRunner.query(`DROP TABLE "temporary_vehicle"`);
+  }
+}
--- src/vehicle/entities/vehicle.entity.ts
+++ src/vehicle/entities/vehicle.entity.ts
@@ -1,11 +1,12 @@
 import { Column, Entity, JoinColumn, OneToMany, OneToOne } from 'typeorm';
 
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
 import { CommonEntity } from '../../common/entities/common.entity';
 import { VehicleType } from '../enums';
 import { Operation } from '../../operations/entities/operation.entity';
 import { Driver } from '../../driver/entities/driver.entity';
 import { Trailer } from './trailer.entity';
+import { IVehicleTank } from '../types';
 
 @Entity()
 export class Vehicle extends CommonEntity {
@@ -25,6 +26,24 @@ export class Vehicle extends CommonEntity {
   @ApiProperty({ required: true, description: 'Регистрационный номер ТС' })
   @Column({ type: 'varchar', nullable: false })
   regNumber: string;
+
+  @ApiProperty({
+    type: () => PickType(IVehicleTank, ['index', 'volume']),
+    required: false,
+    description: 'Объект, содержащий номер и состояние резервуаров',
+    isArray: true,
+  })
+  @Column({ type: 'text', nullable: true })
+  currentState?: string;
+
+  @ApiProperty({
+    type: () => PickType(IVehicleTank, ['index', 'volume']),
+    required: false,
+    description: 'Объект, содержащий номер и калибр резервуара',
+    isArray: true,
+  })
+  @Column({ type: 'text', nullable: true })
+  sectionVolumes?: string;
 
   @ApiProperty({ required: true, description: 'Доступность' })
   @Column({ type: 'boolean', default: true })
