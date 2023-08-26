8671b378033ecc012ccba3e95dfcd72d2c7a310e
artursirbiladze <xjesterx404@gmail.com>
2023-08-26 08:38:58 +0300

    fix migration

--- src/migrations/1692957834831-AddTankStateInShift.ts
+++ src/migrations/1692957834831-AddTankStateInShift.ts
@@ -9,7 +9,7 @@ export class AddTankStateInShift1692957834831 implements MigrationInterface {
             `CREATE TABLE "temporary_shift" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "createdAt" integer NOT NULL DEFAULT (strftime('%s', 'now')), "updatedAt" integer NOT NULL DEFAULT (strftime('%s', 'now')), "deletedAt" datetime, "startedAt" integer NOT NULL, "closedAt" integer, "userId" integer NOT NULL, "startDispensersState" text, "finishDispensersState" text, "finishTankState" text, CONSTRAINT "FK_d6c3886ef9888f23e6d995d2640" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`,
         );
         await queryRunner.query(
-            `INSERT INTO "temporary_shift"("id", "createdAt", "updatedAt", "deletedAt", "startedAt", "closedAt", "userId", "startDispensersState", "finishDispensersState", "finishTankState") SELECT "id", "createdAt", "updatedAt", "deletedAt", "startedAt", "closedAt", "userId", "startDispensersState", "finishDispensersState" FROM "shift"`,
+            `INSERT INTO "temporary_shift"("id", "createdAt", "updatedAt", "deletedAt", "startedAt", "closedAt", "userId", "startDispensersState", "finishDispensersState", "finishTankState") SELECT "id", "createdAt", "updatedAt", "deletedAt", "startedAt", "closedAt", "userId", "startDispensersState", "finishDispensersState", "finishTankState" FROM "shift"`,
         );
         await queryRunner.query(`DROP TABLE "shift"`);
         await queryRunner.query(`ALTER TABLE "temporary_shift" RENAME TO "shift"`);
