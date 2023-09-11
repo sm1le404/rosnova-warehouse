b373d9d761cfa51f82dc6af3e7cae42687f047c5
artursirbiladze <xjesterx404@gmail.com>
2023-09-11 14:09:48 +0300

    fix kakfa mess

--- src/kafka/entities/kafka.message.entity.ts
+++ src/kafka/entities/kafka.message.entity.ts
@@ -1,18 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 import { ApiProperty } from '@nestjs/swagger';
 import { CommonEntity } from '../../common/entities/common.entity';
 
 @Entity()
 export class KafkaMessage extends CommonEntity {
-  @ApiProperty({ description: 'Идентификатор' })
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @ApiProperty({ description: 'Дата создания' })
-  @Column({ type: 'integer', default: () => `strftime('%s', 'now')` })
-  createdAt: number;
-
   @ApiProperty({
     required: true,
     description: 'Данные для отправки',
