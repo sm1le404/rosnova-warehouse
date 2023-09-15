1c9f16f56e35ac85259f5b8aefe3fe2ee6598f6d
artursirbiladze <xjesterx404@gmail.com>
2023-09-15 13:18:46 +0300

    fix отчета pt.2

--- src/report/utils/outcome-report-mapper.util.ts
+++ src/report/utils/outcome-report-mapper.util.ts
@@ -12,6 +12,7 @@ const orderOfColumns = [
   'docWeight',
   'vehicle.regNumber',
   'numberTTN',
+  'fuelName',
 ];
 
 export const outcomeReportMapper = (operations: Operation[]): string[][] => {
@@ -29,6 +30,10 @@ export const outcomeReportMapper = (operations: Operation[]): string[][] => {
       }
       if (key.includes('vehicle.regNumber')) {
         return operation.vehicle ? operation.vehicle.regNumber : '';
+      }
+      if (key.includes('fuelName')) {
+        // eslint-disable-next-line max-len
+        return `${operation.fuel.name} ${operation.refinery.shortName} ${operation.fuelHolder.shortName}`;
       }
       return operation[key] ?? '';
     });
