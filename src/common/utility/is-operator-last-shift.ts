46bf973f6a065d348e9df359b91ffb2dd5369c8d
Denis <denelvis@yandex.ru>
2024-01-17 16:29:57 +0300

    fix: update operation method

---
+++ src/common/utility/is-operator-last-shift.ts
@@ -0,0 +1,7 @@
+import { RoleType } from '../../user/enums';
+
+export const isOperatorLastShift = (
+  role: RoleType,
+  currentShift: number,
+  lastShift: number,
+): boolean => role === RoleType.OPERATOR && currentShift === lastShift;
