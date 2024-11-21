5507a39634b5727273f822f14290d101a3259763
artursirbiladze <xjesterx404@gmail.com>
2024-11-21 20:06:30 +0300

    fix user update

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
-import { Repository } from 'typeorm';
+import { DeepPartial, FindOneOptions, Repository } from 'typeorm';
 import { CommonService } from '../../common/services/common.service';
+import { EncryptionService } from '../../auth/services/encryption.service';
 
 @Injectable()
 export class UserService extends CommonService<User> {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    private readonly encryptionService: EncryptionService,
   ) {
     super();
   }
 
   getRepository(): Repository<User> {
     return this.userRepository;
+  }
+
+  async update(
+    filter: FindOneOptions<User>,
+    updateCommon: DeepPartial<User>,
+  ): Promise<User> {
+    const common = await this.findOne(filter);
+    if (updateCommon?.password) {
+      updateCommon.password = await this.encryptionService.hash(
+        updateCommon.password,
+      );
+    }
+    Object.assign(common, updateCommon);
+    return this.getRepository().save(common);
   }
 }
